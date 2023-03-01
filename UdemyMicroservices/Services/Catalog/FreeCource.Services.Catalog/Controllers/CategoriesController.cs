e9e970e2287408f1e647922f59c14cba42c77cd2
Osman Abay <osmanabay94@gmail.com>
2023-03-01 14:40:39 +0300

    Added Category and Course Controller

---
+++ UdemyMicroservices/Services/Catalog/FreeCource.Services.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,42 @@
+﻿using FreeCource.Services.Catalog.Dtos;
+using FreeCource.Services.Catalog.Services;
+using FreeCourse.Shared.ControllerBases;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCource.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : CustomBaseController
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+
+            return CreateActionResultInstance(categories);
+        }
+
+        public async Task<IActionResult>GetById(string id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+
+            return CreateActionResultInstance(category);
+        }
+
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+            var createdCategory = await _categoryService.CreateAsync(categoryDto);
+
+            return CreateActionResultInstance(createdCategory);
+        }
+    }
+}
